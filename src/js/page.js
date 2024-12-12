bab06faab3ab47b377b9cf5d6768f0b726285564
KOCAOGLU Manolia <157006573+manolia-kocaoglu@users.noreply.github.com>
2024-12-12 10:39:14 +0100

    refactor header and hero styles; update carousel functionality and add new buttons

--- src/js/page.js
+++ src/js/page.js
@@ -1,6 +1,42 @@
-const toggle = document.querySelector(".menu-btn");
+const toggle = document.querySelector(".header__menu-btn");
 const nav = document.querySelector(".menu");
 
 toggle.addEventListener("click", () => {
    nav.classList.toggle("open");
 });
+
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+const premierItem = document.querySelector(".carousel__card");
+const scrollAmount = premierItem.clientWidth;
+
+if (carousel) {
+
+    prevButton.addEventListener("click", () => {
+      carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });;
+    });
+  
+    // Scroll au clic sur le bouton suivant
+    nextButton.addEventListener("click", () => {
+      carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });;
+    });
+  }
+
+const carousel2 = document.querySelector(".carousel__container2");
+const prevButton2 = document.querySelector(".carousel__button--prev2");
+const nextButton2 = document.querySelector(".carousel__button--next2");
+const premierItem2 = document.querySelector(".carousel__card2");
+const scrollAmount2 = premierItem.clientWidth;
+
+if (carousel2) {
+
+    prevButton2.addEventListener("click", () => {
+      carousel2.scrollBy({ left: -scrollAmount2, behavior: "smooth" });;
+    });
+  
+    // Scroll au clic sur le bouton suivant
+    nextButton2.addEventListener("click", () => {
+      carousel2.scrollBy({ left: +scrollAmount2, behavior: "smooth" });;
+    });
+  }
